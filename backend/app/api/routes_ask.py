5a3fa81143edea694afbc5596b5586f943418e03
Moneshai2004 <moneshshanmugam@gmail.com>
2025-11-13 23:57:30 +0530

    API:upload,ask

---
+++ backend/app/api/routes_ask.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from backend.app.core.rag_engine import ask_question
+
+router = APIRouter(prefix="/ask", tags=["Ask"])
+
+class query(BaseModel):
+    question: str   
+
+@router.post("/")
+async def ask(query: query):
+    try:
+        answer = ask_question(query.question)
+        return {"question": query.question, "answer": answer}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.app.api import routes_upload, routes_ask
 
-app=FastAPI(title= "AI chat Assistant - Backend")
+app = FastAPI(title="AI Knowledge Assistant - Backend")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(routes_upload.router)
+app.include_router(routes_ask.router)
+
 @app.get("/")
-async def root():
-    return {"message": "Welcome to the AI chat Assistant Backend!"}
+def root():
+    return {"status": "ok", "message": "Backend running fine"}
